b7b14c187dad9441131935d82015acc372f94a2f
Makhlouf Yassine <yassinemakhlouf5@gmail.com>
2025-04-01 10:48:52 +0100

    enable all requests for now

---
+++ src/main/java/org/green_building/excellent_training/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package org.green_building.excellent_training.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // Disable authentication
+            .csrf(csrf -> csrf.disable()); // Disable CSRF protection
+        return http.build();
+    }
+}
